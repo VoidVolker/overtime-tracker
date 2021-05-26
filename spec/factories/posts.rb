656b2c28944f3dabf27d3838c2b046a6ee574b97
VoidVolker <>
2021-05-26 13:12:21 +0300

    FactoryGirl -> FactoryBot migration

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,13 +1,13 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :post do
-    date Date.today
-    rationale 'Some rationale for post'
-    user_id 1
+    date { Date.today }
+    rationale { 'Some rationale for post' }
+    user_id { 1 }
   end
 
   factory :second_post, class: 'Post' do
-    date Date.yesterday
-    rationale 'Some rationale for second post'
-    user_id 1
+    date { Date.yesterday }
+    rationale { 'Some rationale for second post' }
+    user_id { 1 }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,17 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :user do
-    first_name 'Alice'
-    last_name 'Carroll'
-    email 'test@test.com'
-    password '123456'
-    password_confirmation '123456'
+    first_name { 'Alice' }
+    last_name { 'Carroll' }
+    email { 'test@test.com' }
+    password { '123456' }
+    password_confirmation { '123456' }
   end
 
   factory :admin_user, class: 'AdminUser' do
-    first_name 'Admin'
-    last_name 'User'
-    email 'admin@user.com'
-    password '123456'
-    password_confirmation '123456'
+    first_name { 'Admin' }
+    last_name { 'User' }
+    email { 'admin@user.com' }
+    password { '123456' }
+    password_confirmation { '123456' }
   end
 end
