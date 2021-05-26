751d5a4d854b7407aebc337046366cf4a8eca013
VoidVolker <>
2021-05-26 13:00:29 +0300

    FactoryGirl factories for user and post

---
+++ spec/factories/posts.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :post do
+    date Date.today
+    rationale 'Some rationale for post'
+    user_id 1
+  end
+
+  factory :second_post, class: 'Post' do
+    date Date.yesterday
+    rationale 'Some rationale for second post'
+    user_id 1
+  end
+end
