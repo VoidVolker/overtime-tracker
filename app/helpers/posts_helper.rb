156278cd4bf149e116d666934b4a89808accfa51
VoidVolker <>
2021-05-30 01:31:03 +0300

    Styled show page for post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -7,16 +7,16 @@ module PostsHelper
 
   # <span class="uk-width-1-1 uk-align-center <%= @status_styles[post.status] %>"><%= post.status.camelize %></span>
 
-  def status_label status
-    content_tag(:span, status, class: PostsHelper.status_styles[status])
+  def status_label status, tag_class = ''
+    content_tag(:span, status, class: "#{PostsHelper.status_styles[status]} #{tag_class}")
   end
 
   private
     def self.status_styles
       Hash[
-        'submitted' => 'uk-width-1-1 uk-align-center uk-label',
-        'approved' => 'uk-width-1-1 uk-align-center uk-label uk-label-success',
-        'rejected' => 'uk-width-1-1 uk-align-center uk-label uk-label-warning'
+        'submitted' => 'uk-label',
+        'approved' => 'uk-label uk-label-success',
+        'rejected' => 'uk-label uk-label-warning'
       ]
     end
 end
