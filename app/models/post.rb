bf4262b3d5133410c2a927035dccacbfa171a85c
VoidVolker <>
2021-06-08 19:50:14 +0300

    Implemented callback to update audit log when a post is created

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,19 @@ class Post < ApplicationRecord
   scope :with_status, ->(status) { where(status: Post.statuses[status]) }
   scope :posts_by, ->(user) { where(user_id: user.id) }
 
+  after_save :update_audit_log
+
   def to_json(options={})
     options[:except] ||= [:user_id]
     super(options)
   end
+
+  private
+
+    def update_audit_log
+      audit_log = AuditLog.where(user_id: self.user_id, started: (self.date - 7.days..self.date)).last
+      if audit_log
+        audit_log.confirmed!
+      end
+    end
 end
