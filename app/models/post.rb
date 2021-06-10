7b08cb9640812ea0719984cb3f938d1e6018104e
VoidVolker <>
2021-06-10 18:54:46 +0300

    Code styl small fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,8 +18,6 @@ class Post < ApplicationRecord
 
     def update_audit_log
       audit_log = AuditLog.where(user_id: self.user_id, started: (self.date - 7.days..self.date)).last
-      if audit_log
-        audit_log.confirmed!
-      end
+      audit_log.confirmed! if audit_log
     end
 end
