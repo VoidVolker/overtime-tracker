297955e56352bb86314027b33eab6c3711bd3092
VoidVolker <>
2021-06-12 16:49:14 +0300

    Mailer config update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,5 +22,12 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.port =           ENV['SPARKPOST_SMTP_PORT'],
+  config.action_mailer.address =        ENV['SPARKPOST_SMTP_HOST'],
+  config.action_mailer.user_name =      ENV['SPARKPOST_SMTP_USERNAME'],
+  config.action_mailer.password =       ENV['SPARKPOST_SMTP_PASSWORD'],
+  config.action_mailer.authentication = :login,
+  config.action_mailer.enable_starttls_auto = true
   config.action_mailer.default_url_options = { :host => 'example.com' }
 end
